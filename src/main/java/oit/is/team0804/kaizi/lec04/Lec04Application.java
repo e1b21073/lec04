a724d00b2a0b54f0a53040467ec7a5f0ded98558
e1b21073 <e1b21073@oit.ac.jp>
2023-10-17 14:02:21 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team0804/kaizi/lec04/Lec04Application.java
@@ -0,0 +1,13 @@
+package oit.is.team0804.kaizi.lec04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec04Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team0804/kaizi/lec04/Lec04ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team0804.kaizi.lec04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
