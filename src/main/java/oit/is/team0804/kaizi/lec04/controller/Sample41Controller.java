99b5c51f629064cdc344aaa6e5ccf9392327da55
koichi-shirakashi <e1b21050@oit.ac.jp>
2023-10-17 16:01:15 +0900

    sample45.htmlを表示するGETリクエストを実装

--- src/main/java/oit/is/team0804/kaizi/lec04/controller/Sample41Controller.java
+++ src/main/java/oit/is/team0804/kaizi/lec04/controller/Sample41Controller.java
@@ -34,6 +34,11 @@ public class Sample41Controller {
     return "sample43.html";
   }
 
+  @GetMapping("step5")
+  public String sample45() {
+    return "sample45.html";
+  }
+
   @GetMapping("step2/{id}")
   public String sample42(@PathVariable Integer id, ModelMap model) {
     Chamber chamber2 = chamberMapper.selectById(id);
